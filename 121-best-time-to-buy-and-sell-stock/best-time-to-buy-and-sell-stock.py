431d4c77fef9a894885b3e88a028f07130bea0b9
DeekshaHU <deekshashettyhu@gmail.com>
2026-09-14 18:52:03 +0530

    Time: 35 ms (79.66%) | Memory: 28.7 MB (42.24%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -11,3 +11,17 @@ class Solution:
                 max_profit = max(max_profit, profit)
         
         return max_profit
+
+        x=min(prices)
+        minp=prices[0]
+        imin=0
+        for i in range(len(arr)):
+            if prices[i]<minp:
+                minp=prices[0]
+                imin=i
+        l=prices[imin:]
+        maxp=l[0]
+        for i  in range(len(l)):
+            if l[i]>maxp:
+                maxp=l[i]
+        return maxp-minp
